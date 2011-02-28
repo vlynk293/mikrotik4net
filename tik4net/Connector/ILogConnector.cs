32e2903a6602c1a52c07d05491d19e73f482e9a8
daniel.frantik <daniel.frantik@gmail.com>
2011-02-28 20:01:47 +0000

    Log management.

---
+++ tik4net/Connector/ILogConnector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tik4Net.Objects;
+
+namespace Tik4Net.Connector
+{
+    /// <summary>
+    /// Interface implemented by <see cref="ITikConnector"/> that supports log management via <see cref="Log(string,LogLevel)"/>.
+    /// </summary>
+    public interface ILogConnector: ITikConnector
+    {
+        /// <summary>
+        /// Writes specified <paramref name="message"/> to log with <paramref name="level"/>.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="level">The log level.</param>
+        void Log(string message, LogLevel level);
+    }
+}
---
+++ tik4net/Objects/LogLevel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tik4Net.Objects
+{
+    /// <summary>
+    /// Log level in <see cref="Tik4Net.Connector.ILogConnector.Log"/>.
+    /// </summary>
+    /// <seealso cref="Tik4Net.Connector.ILogConnector"/>
+    public enum LogLevel
+    {
+        /// <summary>
+        /// Debug level.
+        /// </summary>
+        Debug,
+        /// <summary>
+        /// Info level.
+        /// </summary>
+        Info,
+        /// <summary>
+        /// Warning level.
+        /// </summary>
+        Warning,
+        /// <summary>
+        /// Error level.
+        /// </summary>
+        Error,
+    }
+}
